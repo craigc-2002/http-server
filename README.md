07e60011ef7a5e9f21f49e3153eb046785ad35d9
Craig Cochrane <craig@cochrane.cc>
2023-12-19 19:36:19 +0000

    Added option to run the server continuously.
    Continuous operation can be enabled with the -c command line option.
    The getopt library function is used to parse the command line options.

--- README.md
+++ README.md
@@ -4,9 +4,18 @@ Very basic minimalist HTTP server. Based on [this video](https://www.youtube.com
 
 It has no security or error checking, so don't use it.
 
+## Usage
+```./server [options] [port number]```
+
+### Options
+- -c : Run server continously. If not given, the server only replies to a single request and then exits
+
+### Port number
+A port number can optionally be added after the command line options. If it is not given, the port number defaults to 8080.
+
 ## To Do
-- remove debugging output and add proper logging
+- remove debugging output and add proper logging - use syslog library
 - Limit files that can be served (maybe add a set folder that is searched for files)
-- Server runs continously until process stopped
 - build directory for build artifacts
 - update readme
+- implement support for other http requests
--- server.c
+++ server.c
@@ -5,17 +5,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "libserver.h"
 
 int main(int argc, char* argv[])
 {
+    // parse arguments using getopt to check if the user wants the server to run continuously
+    bool run_continuous = false;
+    int opt;
+    extern int optind;
+
+    while ((opt = getopt(argc, argv, "c")) != -1)
+    {
+        switch (opt)
+        {
+            case 'c':
+                run_continuous = true;
+                break;
+
+            default:
+                fprintf(stderr, "Usage: ./server [-c (run continously)] [port_num]");
+                exit(-1);
+        }
+    }
+
     // check command line arguments to see if a port number has been specified, if not default to 8080
     uint16_t port_num = 8080;
 
-    if (argc > 1)
+    if (optind < argc)
     {
-        port_num = atoi(argv[1]);
+        port_num = atoi(argv[optind]);
     }
 
     printf("Server Started on port %d\n\n", port_num);
@@ -39,83 +59,88 @@ int main(int argc, char* argv[])
     bind(s, (const struct sockaddr*) &addr, sizeof(addr));
     listen(s, 10); // set socket to listen for incoming connections
 
-    // accept the first connection
-    int client_fd = accept(s, 0, 0);
-    if (client_fd < 0)
+    do
     {
-        exit(-1);
-    }
-
-    // receive HTTP request from client into a buffer (if request is more than 255 bytes it will be truncated, only the requested file is actually read)
-    char buf[256] = {0};
-    recv(client_fd, buf, 255, 0);
-    printf("%s\n", buf);
-
-    // parse HTTP request
-    http_reply_t reply = parse_http_request(buf);
-
-    // construct correct HTTP response depending on the reply status code
-    char* response;
-
-    switch (reply.status_code)
-    {
-        case 200:
+        // accept the first connection
+        int client_fd = accept(s, 0, 0);
+        if (client_fd < 0)
         {
-            // request was successful, return the requested resource
+            exit(-1);
+        }
 
-            // open requested file
-            FILE* requested_file = fopen(reply.requested_file, "r");
+        // receive HTTP request from client into a buffer (if request is more than 255 bytes it will be truncated, only the requested file is actually read)
+        char buf[256] = {0};
+        recv(client_fd, buf, 255, 0);
+        printf("%s\n", buf);
 
-            // check that the requested file is opened successfully
-            if (requested_file == NULL)
+        // parse HTTP request
+        http_reply_t reply = parse_http_request(buf);
+
+        // construct correct HTTP response depending on the reply status code
+        char* response;
+
+        switch (reply.status_code)
+        {
+            case 200:
             {
-                fprintf(stderr, "Requested file can't be opened");
-                exit(-1);
+                // request was successful, return the requested resource
+
+                // open requested file
+                FILE* requested_file = fopen(reply.requested_file, "r");
+
+                // check that the requested file is opened successfully
+                if (requested_file == NULL)
+                {
+                    fprintf(stderr, "Requested file can't be opened");
+                    exit(-1);
+                }
+
+                // save contents of requested file into buffer
+                fseek(requested_file, 0, SEEK_END); // go to end of file to get the size
+                int file_length = ftell(requested_file); // position of stream at end of file gives the length
+                fseek(requested_file, 0, SEEK_SET);
+
+                char* req_file_buf = malloc(file_length+1); // buffer to hold file contents + null terminator
+                fread(req_file_buf, sizeof(char), file_length+1, requested_file); // read file contents into buffer 1 byte at a time
+                req_file_buf[file_length] = 0;
+                fclose(requested_file);
+                printf("%s\n", req_file_buf);
+
+                // form HTTP response with file contents
+                response = http_response(200, req_file_buf);
+                break;
             }
 
-            // save contents of requested file into buffer
-            fseek(requested_file, 0, SEEK_END); // go to end of file to get the size
-            int file_length = ftell(requested_file); // position of stream at end of file gives the length
-            fseek(requested_file, 0, SEEK_SET);
+            case 400:
+            {
+                // request was not correctly formed
+                response = http_response(400, "Malformed request");
+                break;
+            }
 
-            char* req_file_buf = malloc(file_length+1); // buffer to hold file contents + null terminator
-            fread(req_file_buf, sizeof(char), file_length+1, requested_file); // read file contents into buffer 1 byte at a time
-            req_file_buf[file_length] = 0;
-            fclose(requested_file);
-            printf("%s\n", req_file_buf);
+            case 404:
+            {
+                // request did not ask for a valid file
+                response = http_response(404, "404 Not Found");
+                break;
+            }
 
-            // form HTTP response with file contents
-            response = http_response(200, req_file_buf);
-            break;
+            default:
+            {
+                // the reply doesn't have a status code handled above, return a server error
+                response = http_response(500, "Server error");
+            }
         }
 
-        case 400:
-        {
-            // request was not correctly formed
-            response = http_response(400, "Malformed request");
-            break;
-        }
 
-        case 404:
-        {
-            // request did not ask for a valid file
-            response = http_response(404, "404 Not Found");
-            break;
-        }
+        printf("\nHTTP Response:\n%s\n", response);
+        // send response to client
+        write(client_fd, response, strlen(response));
 
-        default:
-        {
-            // the reply doesn't have a status code handled above, return a server error
-            response = http_response(500, "Server error");
-        }
-    }
+        free(response);
+        close(client_fd);
 
+    }while (run_continuous);
 
-    printf("\nHTTP Response:\n%s\n", response);
-    // send response to client
-    write(client_fd, response, strlen(response));
-
-    free(response);
-    close(client_fd);
     close(s);
 }
